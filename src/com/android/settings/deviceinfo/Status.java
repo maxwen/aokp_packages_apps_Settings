android/platform_packages_apps_settings

Format change at "Mobile network type" value
Remove "network type Number" from "mobile network type" value
at "About Phone > Status".

For example value "UNKNOWN:0" became "UNKNOWN"
(if value is "UNKNOWN", the word is localized)

The number ":0" did not add much value to the user.

Change-Id: Iee29fa2ab42f2d6eab5d9dbd6f647087ee382648

src/com/android/settings/deviceinfo/Status.java
@@ -332,8 +332,11 @@ public class Status extends PreferenceActivity {
 
     private void updateNetworkType() {
         // Whether EDGE, UMTS, etc...
-        setSummaryText(KEY_NETWORK_TYPE, mTelephonyManager.getNetworkTypeName() +
-                ":" + mTelephonyManager.getNetworkType());
+        String networktype = null;
+        if (mTelephonyManager.NETWORK_TYPE_UNKNOWN != mTelephonyManager.getNetworkType()) {
+            networktype = mTelephonyManager.getNetworkTypeName();
+        }
+        setSummaryText(KEY_NETWORK_TYPE, networktype);
     }
 
     private void updateDataState() {

maxwen/aokp_packages_apps_Settings

src/com/android/settings/deviceinfo/Status.java
@@ -332,8 +332,11 @@ public class Status extends PreferenceActivity {
 
     private void updateNetworkType() {
         // Whether EDGE, UMTS, etc...
-        setSummaryText(KEY_NETWORK_TYPE, mTelephonyManager.getNetworkTypeName() +
-                ":" + mTelephonyManager.getNetworkType());
+        String networktype = null;
+        if (TelephonyManager.NETWORK_TYPE_UNKNOWN != mTelephonyManager.getNetworkType()) {
+            networktype = mTelephonyManager.getNetworkTypeName();
+        }
+        setSummaryText(KEY_NETWORK_TYPE, networktype);
     }
 
     private void updateDataState() {